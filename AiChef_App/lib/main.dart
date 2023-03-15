06207d91a77f5cd5f9a0341d0cbde57c62a2f4e3
BhaumikPatel1609 <patelbhaumik477@gmail.com>
2023-03-15 14:25:38 +0530

    add text function

--- AiChef_App/lib/main.dart
+++ AiChef_App/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Hello Bhaumik',
             ),
             Text(
               '$_counter',
@@ -105,11 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
